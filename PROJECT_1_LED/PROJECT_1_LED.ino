94a8189f09bb5171536df520f8bec58265c761a7
ThanhTung281002 <94810095+ThanhTung281002@users.noreply.github.com>
2025-06-23 09:28:15 +0700

    Thêm code cho hướng dẫn buổi 1 led nhấp nháy

---
+++ PROJECT_1_LED/PROJECT_1_LED.ino
@@ -0,0 +1,29 @@
+/*****************************************************
+
+  HỌ VÀ TÊN: 
+  TRƯỜNG: 
+  LỚP: 
+
+
+*****************************************************/
+
+
+void setup() {
+  // CODE TRONG NÀY SẼ CHỈ CHẠY MỘT LẦN 
+  // set up cho chân sẽ sử dụng trên arduino là output 
+
+}
+
+void loop() {
+  // CODE TRONG NÀY SẼ CHẠY LẶP LẠI LIÊN TỤC 
+  // đèn sáng 
+  
+  // dừng 500ms
+
+  // đèn tắt 
+
+  // dừng 500ms
+
+}
+
+
